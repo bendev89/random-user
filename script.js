888b509a273c214d46387f18a6151b9138ab0dd4
Benoit <ndiayeben89@gmail.com>
2021-07-22 04:34:08 +0200

    Fonctionnalité : ajout d'une personne

--- script.js
+++ script.js
@@ -5,22 +5,42 @@ const millionnaireBtn = document.getElementById('millionnaire');
 const sortBtn = document.getElementById('sort');
 const totalBtn = document.getElementById('total');
 
+
 let data =[];
 
 async function getRandomPerson() {
     const res = await fetch('https://randomuser.me/api');
+    ;
     const data = await res.json();
     const person = data.results[0];
+    
     
     const newPerson = {
         name : ` ${person.name.first} ${person.name.last} `,
         image: `${person.picture.thumbnail}`,
         wealth: Math.floor(Math.random() * 1000000)
     }
+    console.log(newPerson);
     addPerson(newPerson);
      
 }
+getRandomPerson();
 
-function addPerson(obj) {
-    data.push(obj)
-}
+ function addPerson(obj) {
+     data.push(obj);
+     updateDom();
+ }
+
+ function updateDom(persons = data) {
+     main.innerHTML = "<h2><strong>Personne</strong> Fortune</h2>";
+     persons.forEach(item => {
+         const element = document.createElement('div');
+         element.classList.add('person')
+         element.innerHTML =`<div><img src="${item.image}" >
+          <strong>${item.name}</strong></div> 
+          <p>${item.wealth} €</p>`;
+          main.appendChild(element);
+
+     });
+ }
+ addPersonBtn.addEventListener('click', getRandomPerson);
